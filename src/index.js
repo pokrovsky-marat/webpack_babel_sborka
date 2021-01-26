c2fb80263cdf084e809c75f7e6001e8374435701
Marat Isaev <fake@mail.com>
2021-01-26 21:10:13 +0600

    Poka vsyo rabotaet

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./main.scss"
+import "./main.scss";
 
-const App = () => <h1>Hello, World!</h1>;
+const App = () => <h1>Hello, World. Я твой жена уважал</h1>;
 ReactDOM.render(<App />, document.getElementById("root"));
