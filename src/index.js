28f1c1757c762d9690e00343fd412123943d96dd
Marat Isaev <fake@mail.com>
2021-01-27 15:21:09 +0600

    final commit

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./main.scss";
 
-const App = () => <h1>Hello, World. Я твой жена уважал</h1>;
+const App = () => (
+  <h1>
+    {" "}
+    <span>жена</span> Я твой уважал
+  </h1>
+);
 ReactDOM.render(<App />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,70 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-module.exports = {
-  mode: "development",
-  module: {
-    rules: [
-      {
-        //For js files
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-      },
-      //For css files
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      //For SCSS and SASS
-      {
-        // Регулярное выражение которое позволяет выбрать как .scss, так и .sass
-        test: /\.s[ca]ss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-      //For images
-      {
-        test: /\.(png|gif|jpg|jpeg|ico)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              outputPath: "images",
-              name: "[name]-[sha1:hash:7].[ext]",
+
+module.exports = (env) => {
+  const { mode = "development" } = env;
+  getStyleLoader = () =>
+    mode === "development" ? "style-loader" : MiniCssExtractPlugin.loader;
+
+  return {
+    mode: mode,
+    module: {
+      rules: [
+        {
+          //For js files
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: "babel-loader",
+        },
+        //For css files
+        {
+          test: /\.css$/,
+          use: [getStyleLoader(), "css-loader"],
+        },
+        //For SCSS and SASS
+        {
+          // Регулярное выражение которое позволяет выбрать как .scss, так и .sass
+          test: /\.s[ca]ss$/,
+          use: [getStyleLoader(), "css-loader", "sass-loader"],
+        },
+        //For images
+        {
+          test: /\.(png|gif|jpg|jpeg|ico)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                outputPath: "images",
+                name: "[name]-[sha1:hash:7].[ext]",
+              },
             },
-          },
-        ],
-      },
-      //For fonts
-      {
-        test: /\.(woff|woff2|ttf|eot|otf)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              outputPath: "fonts",
-              name: "[name].[ext]",
+          ],
+        },
+        //For fonts
+        {
+          test: /\.(woff|woff2|ttf|eot|otf)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                outputPath: "fonts",
+                name: "[name].[ext]",
+              },
             },
-          },
-        ],
-      },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "public/index.html",
+        title: "Name from webpack plugin",
+        buildTime: new Date().toISOString(),
+      }),
+      new MiniCssExtractPlugin(),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "public/index.html",
-      title: "Name from webpack plugin",
-      buildTime: new Date().toISOString(),
-    }),
-    new MiniCssExtractPlugin(),
-  ],
-  devServer: {
-    open: true,
-  },
+    devServer: {
+      open: true,
+    },
+  };
 };
